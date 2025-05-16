a821da304eb74a5929bb23b757b93173c1a85d5b
euning <52948715+eunee99@users.noreply.github.com>
2025-05-16 20:14:21 +0900

    CMSISv1

--- de/task.c
+++ de/task.c
@@ -1,126 +1,138 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "cmsis_os2.h"
+#include "cmsis_os.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "task.h"
+#include "semphr.h"
 
-/* Task Priorities ----------------------------------------------------------*/
-#define TASK_GEN_PRIO   6
-#define TASK_COMP_PRIO  6
-#define TASK_PCTX_PRIO  8
+/* Priorities */
+#define TASK_GEN1_PRIO   6
+#define TASK_GEN2_PRIO   6
+#define TASK_COMP_PRIO   6
+#define TASK_PCTX_PRIO   8
 #define TASK_PCCMD_PRIO 10
 
-/* RTOS Handles -------------------------------------------------------------*/
-osThreadId_t tidGen1, tidGen2, tidComp, tidPcTx, tidPcCmd;
-osMutexId_t uartMutex;
-osMessageQueueId_t sensorQueue;
-osMessageQueueId_t compInputQueue;
-osSemaphoreId_t notifySem;
+/* Handles */
+TaskHandle_t xHandleGen1, xHandleGen2, xHandleComp, xHandlePcCmd, xHandlePcTx;
+QueueHandle_t sensorQueue;
+QueueHandle_t compInputQueue;
+SemaphoreHandle_t uartMutex;
 
-/* External UART Handle -----------------------------------------------------*/
 extern UART_HandleTypeDef huart1;
 
-/* Data Structure -----------------------------------------------------------*/
+/* Data structure */
 typedef struct {
     int sensor_id;
     unsigned long timestamp;
     int data;
 } SensorData;
 
-/* Global Variables ---------------------------------------------------------*/
-static SensorData latestSensor1 = {1, 0, 0};
-static SensorData latestSensor2 = {2, 0, 0};
-static uint8_t rxByte;
-static uint8_t cmdBuffer[16];
-static uint8_t cmdIndex = 0;
-
-/* Function Prototypes ------------------------------------------------------*/
-static void TaskGen(void *pvParameters);
+/* Function Prototypes */
+static void TaskGen1(void *pvParameters);
+static void TaskGen2(void *pvParameters);
 static void TaskComp(void *pvParameters);
-static void TaskPcTx(void *pvParameters);
-static void TaskPcCmd(void *pvParameters);
+static void TaskpcCmd(void *pvParameters);
+static void TaskpcTx(void *pvParameters);
 
-/* RTOS Thread Initialization -----------------------------------------------*/
+/* Globals */
+SensorData latestSensor1 = {1, 0, 0};
+SensorData latestSensor2 = {2, 0, 0};
+uint8_t rxByte;
+uint8_t cmdBuffer[16];
+uint8_t cmdIndex = 0;
+
 void USER_THREADS(void) {
-    sensorQueue = osMessageQueueNew(10, sizeof(SensorData), NULL);
-    compInputQueue = osMessageQueueNew(2, sizeof(SensorData), NULL);
-    uartMutex = osMutexNew(NULL);
-    notifySem = osSemaphoreNew(1, 0, NULL);
+    sensorQueue = xQueueCreate(10, sizeof(SensorData));
+    compInputQueue = xQueueCreate(2, sizeof(SensorData));
+    uartMutex = xSemaphoreCreateMutex();
 
-    tidGen1 = osThreadNew(TaskGen, (void*)1, NULL);
-    tidGen2 = osThreadNew(TaskGen, (void*)2, NULL);
-    tidComp = osThreadNew(TaskComp, NULL, NULL);
-    tidPcCmd  = osThreadNew(TaskPcCmd, NULL, NULL);
-    tidPcTx   = osThreadNew(TaskPcTx, NULL, NULL);
+    xTaskCreate(TaskGen1, "TaskGen1", 256, NULL, TASK_GEN1_PRIO, &xHandleGen1);
+    xTaskCreate(TaskGen2, "TaskGen2", 256, NULL, TASK_GEN2_PRIO, &xHandleGen2);
+    xTaskCreate(TaskComp,  "TaskComp", 256, NULL, TASK_COMP_PRIO, &xHandleComp);
+    xTaskCreate(TaskpcCmd, "TaskpcCmd", 256, NULL, TASK_PCCMD_PRIO, &xHandlePcCmd);
+    xTaskCreate(TaskpcTx,  "TaskpcTx", 256, NULL, TASK_PCTX_PRIO, &xHandlePcTx);
 }
 
-/* Sensor Data Generation Task ----------------------------------------------*/
-static void TaskGen(void *pvParameters) {
-    int id = (int)(uintptr_t)pvParameters;
-    SensorData s = { .sensor_id = id };
-
-    for (;;) {
-        osDelay(1000);
-        s.timestamp = osKernelGetTickCount();
+/* Generate random sensor data */
+static void TaskGen1(void *pvParameters) {
+    SensorData s;
+    s.sensor_id = 1;
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        s.timestamp = xTaskGetTickCount();
         s.data = rand() % 100;
-        osMessageQueuePut(sensorQueue, &s, 0, osWaitForever);
+        xQueueSend(sensorQueue, &s, portMAX_DELAY);
     }
 }
 
-/* Sensor Data Comparison Task ----------------------------------------------*/
+static void TaskGen2(void *pvParameters) {
+    SensorData s;
+    s.sensor_id = 2;
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        s.timestamp = xTaskGetTickCount();
+        s.data = rand() % 100;
+        xQueueSend(sensorQueue, &s, portMAX_DELAY);
+    }
+}
+
 static void TaskComp(void *pvParameters) {
     SensorData s;
+    while (1) {
+        if (xQueueReceive(sensorQueue, &s, portMAX_DELAY) == pdPASS) {
+            if (s.sensor_id == 1) {
+                latestSensor1 = s;
+            } else if (s.sensor_id == 2) {
+                latestSensor2 = s;
+            }
 
-    for (;;) {
-        if (osMessageQueueGet(sensorQueue, &s, NULL, osWaitForever) == osOK) {
-            if (s.sensor_id == 1) latestSensor1 = s;
-            else if (s.sensor_id == 2) latestSensor2 = s;
-
-            osMessageQueueReset(compInputQueue);
-            osMessageQueuePut(compInputQueue, &latestSensor1, 0, 0);
-            osMessageQueuePut(compInputQueue, &latestSensor2, 0, 0);
+            xQueueReset(compInputQueue);
+            xQueueSend(compInputQueue, &latestSensor1, 0);
+            xQueueSend(compInputQueue, &latestSensor2, 0);
         }
     }
 }
 
-/* UART Transmission Task ---------------------------------------------------*/
-static void TaskPcTx(void *pvParameters) {
+static void TaskpcTx(void *pvParameters) {
     SensorData s1, s2;
     char msg[64];
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(2000));
 
-    for (;;) {
-        osDelay(2000);
-
-        if (osMessageQueueGetCount(compInputQueue) >= 2) {
-            osMessageQueueGet(compInputQueue, &s1, NULL, 0);
-            osMessageQueueGet(compInputQueue, &s2, NULL, 0);
+        if (uxQueueMessagesWaiting(compInputQueue) >= 2) {
+            xQueueReceive(compInputQueue, &s1, 0);
+            xQueueReceive(compInputQueue, &s2, 0);
 
             int diff = abs(s1.data - s2.data);
             snprintf(msg, sizeof(msg), "DIFF:%d (S1:%d S2:%d)\r\n", diff, s1.data, s2.data);
 
-            osMutexAcquire(uartMutex, osWaitForever);
+            xSemaphoreTake(uartMutex, portMAX_DELAY);
             HAL_UART_Transmit(&huart1, (uint8_t *)msg, strlen(msg), HAL_MAX_DELAY);
-            osMutexRelease(uartMutex);
+            xSemaphoreGive(uartMutex);
         }
     }
 }
 
-/* UART Command Handling Task -----------------------------------------------*/
-static void TaskPcCmd(void *pvParameters) {
+static void TaskpcCmd(void *pvParameters) {
     HAL_UART_Receive_IT(&huart1, &rxByte, 1);
-
-    for (;;) {
-        osSemaphoreAcquire(notifySem, osWaitForever);
+    while (1) {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
         char buffer[64];
-        if (cmdBuffer[0] == 'A') strcpy(buffer, "ACK_FIRE:A\r\n");
-        else if (cmdBuffer[0] == 'B') strcpy(buffer, "ACK_FIRE:B\r\n");
-        else strcpy(buffer, "NACK_FIRE\r\n");
+        if (cmdBuffer[0] == 'A')
+            strcpy(buffer, "ACK_FIRE:A\r\n");
+        else if (cmdBuffer[0] == 'B')
+            strcpy(buffer, "ACK_FIRE:B\r\n");
+        else
+            strcpy(buffer, "NACK_FIRE\r\n");
 
-        osMutexAcquire(uartMutex, osWaitForever);
+        xSemaphoreTake(uartMutex, portMAX_DELAY);
         HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
-        osMutexRelease(uartMutex);
+        xSemaphoreGive(uartMutex);
 
         memset(cmdBuffer, 0, sizeof(cmdBuffer));
         cmdIndex = 0;
@@ -128,12 +140,13 @@ static void TaskPcCmd(void *pvParameters) {
     }
 }
 
-/* UART RX Callback ---------------------------------------------------------*/
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
     if (huart->Instance == USART1) {
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
         if (rxByte == ';') {
             cmdBuffer[cmdIndex] = '\0';
-            osSemaphoreRelease(notifySem);
+            vTaskNotifyGiveFromISR(xHandlePcCmd, &xHigherPriorityTaskWoken);
+            portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
         } else {
             if (cmdIndex < sizeof(cmdBuffer) - 1)
                 cmdBuffer[cmdIndex++] = rxByte;
